e6f4d4112f9407a27e76af2f2f639acd280084e9
Kaan Toraman <91319092+Sisedes@users.noreply.github.com>
2022-07-30 20:59:48 +0300

    Add files via upload

---
+++ Kare-Cember (ALAN CEVRE)/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+
+namespace uy_nasi
+{
+    class Hesap
+    {
+        public double r;
+        public Hesap()
+        { }
+        public double rr
+        { get { return r; } set { r = value; } }
+
+        public void Cevre(double x)
+        {
+
+
+            double cevre = 2 * x * r;
+            Console.WriteLine("Cevre:" + cevre);
+        }
+        public void Cevre()
+        {
+            double cevre = 4 * r;
+            Console.WriteLine("Cevre:" + cevre);
+        }
+        public void Alan()
+        {
+            double alan = r * r;
+            Console.WriteLine("Alan:" + alan);
+        }
+        public void Alan(double x)
+        {
+
+            double alan = x * r * r;
+            Console.WriteLine("Alan:" + alan);
+
+
+        }
+
+    }
+
+    class anaprogram
+    {
+        static void Main(string[] args)
+        {
+            int sec = new int();
+            double hee = 22 / 7;
+
+            Hesap h1 = new Hesap();
+            do
+            {
+                Console.WriteLine("Islem yapilacak sekil seciniz:");
+                Console.WriteLine("1-Kare");
+                Console.WriteLine("2-Daire");
+                sec = Convert.ToInt32(Console.ReadLine());
+            } while (sec != 1 && sec != 2);
+
+
+            Console.WriteLine("Kare kosesi veya cember capi icin deger giriniz:");
+            double gir = new double();
+            gir = Convert.ToDouble(Console.ReadLine());
+            h1.r = gir;
+
+            if (sec == 1)
+            {
+
+                Console.WriteLine("--------KARE--------");
+
+                h1.Cevre();
+                h1.Alan();
+            }
+            else if (sec == 2)
+            {
+                Console.WriteLine("--------Cember--------");
+
+                h1.Cevre(hee);
+                h1.Alan(hee);
+
+            }
+
+
+
+
+
+        }
+    }
+}
---
+++ ListBox Sayı Listeleme(8A)/Odev_8A/Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace Odev_8A
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(82, 36);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(259, 436);
+            this.listBox1.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(581, 228);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(599, 332);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 521);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
